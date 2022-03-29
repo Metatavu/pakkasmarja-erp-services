3fe867b91407a05fb73b608e2d604a03a3bc83e3
Antti Leppä <antti.leppa@metatavu.fi>
2022-03-29 09:26:15 +0300

    Fixes #21

--- src/main/kotlin/fi/metatavu/pakkasmarja/services/erp/config/ConfigController.kt
+++ src/main/kotlin/fi/metatavu/pakkasmarja/services/erp/config/ConfigController.kt
@@ -3,11 +3,7 @@ package fi.metatavu.pakkasmarja.services.erp.config
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import fi.metatavu.pakkasmarja.services.erp.model.GroupProperty
-import org.eclipse.microprofile.config.inject.ConfigProperty
-import java.io.File
 import javax.enterprise.context.ApplicationScoped
-import javax.inject.Inject
-
 
 /**
  * Controller for configurations
@@ -15,18 +11,14 @@ import javax.inject.Inject
 @ApplicationScoped
 class ConfigController {
 
-    @Inject
-    @ConfigProperty(name="fi.metatavu.pakkasmarja.group-codes-file-name")
-    lateinit var groupCodesFileName: String
-
     /**
      * Returns the group properties from config file
      *
      * @return list of group properties
      */
     fun getGroupPropertiesFromConfigFile(): List<GroupProperty> {
-        val file = File(groupCodesFileName)
-        return jacksonObjectMapper().readValue(file)
+        val groupCodes = javaClass.classLoader.getResourceAsStream("group-codes.json") ?: return emptyList()
+        return jacksonObjectMapper().readValue(groupCodes)
     }
 
 }
