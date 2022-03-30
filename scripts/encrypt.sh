426a2220a79944d2a9e3eae0c096f2ce0639e046
Antti Leppä <antti.leppa@metatavu.fi>
2022-03-30 07:13:06 +0300

    Updated secured.tar.gz

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,2 +1,3 @@
 #/bin/sh
-tar -czf - src/test/resources/edm.xml | openssl enc -aes-256-cbc -pass env:ENC_PASS -out secured.tar.gz
+
+tar -czf - src/test/resources/edm.xml | openssl enc -pbkdf2 -aes-256-cbc -iter $ENC_ITER -md sha512 -pass env:ENC_PASS -out secured.tar.gz
